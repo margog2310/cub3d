f67e9090f1c495cee275d01a6fd2dbe3c631b818
margog2310 <margaritaigancheva@gmail.com>
2025-01-28 15:20:56 +0000

    Refactor free_grid function and update author information in libft and main files

--- srcs/libft/free_grid.c
+++ srcs/libft/free_grid.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   free_grid.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
+/*   By: mganchev <mganchev@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 18:49:32 by mganchev          #+#    #+#             */
-/*   Updated: 2025/01/22 17:09:36 by ssottori         ###   ########.fr       */
+/*   Updated: 2025/01/22 20:46:04 by mganchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/libft.h"
 
-// void    free_grid(char **grid, int line_count)
-// {
-//     int i;
+void	free_grid(char **grid, int line_count)
+{
+	int	i;
 
-//     while (i < line_count)
-//     {
-//         free(grid[i]);
-//         i++;
-//     }
-//     free(grid);
-// }
+	i = 0;
+    while (i < line_count)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
